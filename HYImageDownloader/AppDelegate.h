3f9fed4904cf8965b2ae580664722303dab0b73b
Yang9322 <576421791@qq.com>
2016-05-01 14:54:16 +0800

    Initial Commit

---
+++ HYImageDownloader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HYImageDownloader
+//
+//  Created by He yang on 16/5/1.
+//  Copyright © 2016年 He yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HYImageDownloader/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HYImageDownloader
+//
+//  Created by He yang on 16/5/1.
+//  Copyright © 2016年 He yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
