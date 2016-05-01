cf74f7647d4e96fca8ef7694d7efe36dc525ea92
Yang9322 <576421791@qq.com>
2016-05-01 21:28:44 +0800

    FirstCommit

---
+++ HYImageDownloader/UIImageView+HYImageDownloader.h
@@ -0,0 +1,19 @@
+//
+//  UIImageView+HYImageDownloader.h
+//  HYImageDownloader
+//
+//  Created by He yang on 16/5/1.
+//  Copyright © 2016年 He yang. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+
+@interface UIImageView (HYImageDownloader)
+
+
+- (void)hy_setImageWithURLString:(NSString *)URLString;
+
+
+- (void)hy_setImageWithURLString:(NSString *)URLString placeHolder:(UIImage *)placeHolder;
+@end
---
+++ HYImageDownloader/UIImageView+HYImageDownloader.m
@@ -0,0 +1,16 @@
+//
+//  UIImageView+HYImageDownloader.m
+//  HYImageDownloader
+//
+//  Created by He yang on 16/5/1.
+//  Copyright © 2016年 He yang. All rights reserved.
+//
+
+#import "UIImageView+HYImageDownloader.h"
+
+@implementation UIImageView (HYImageDownloader)
+
+
+
+
+@end
