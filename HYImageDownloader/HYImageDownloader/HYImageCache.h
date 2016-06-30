0c6408e207791fa1b906f3ee156c9e5cc3a5ad05
heyang <heyang@juyidemac-mini.local>
2016-06-30 11:03:40 +0800

    Optimize Cache

--- HYImageDownloader/HYImageDownloader/HYImageCache.h
+++ HYImageDownloader/HYImageDownloader/HYImageCache.h
@@ -8,6 +8,57 @@
 
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
+
+
+@interface HYMemoryCache :NSObject
+@property (nonatomic, copy) NSString *name;
+
+@property (nonatomic,readonly) NSUInteger totalCount;
+
+@property (nonatomic,readonly) NSUInteger totalCost;
+
+@property (nonatomic,assign) NSUInteger countLimit;
+
+@property (nonatomic,assign) NSUInteger costLimit;
+
+@property (nonatomic,assign) NSTimeInterval ageLimit;
+
+@property (nonatomic,assign)NSTimeInterval autoTrimInterval;
+
+@property (nonatomic,assign) BOOL shouldRemoveAllObjectsOnMemoryWarning;
+
+@property (nonatomic,assign)BOOL shouldRemoveAllObjectsWhenEnteringBackground;
+
+@property (nonatomic, copy) void(^didReceiveMemoryWarningBlock)(HYMemoryCache *cache);
+
+@property (nonatomic, copy) void(^didEnterBackgroundBlock)(HYMemoryCache *cache);
+
+@property (nonatomic,assign)BOOL releaseOnMainThread;
+
+@property (nonatomic,assign)BOOL releaseAsynchronously;
+
+- (BOOL)containsObjectForKey:(id)key;
+
+- (id)objectForKey:(id)key;
+
+- (void)setObject:(id)object forKey:(id)key;
+
+- (void)setObject:(id)object forKey:(id)key withCost:(NSUInteger)cost;
+
+- (void)removeObjectForKey:(id)key;
+
+- (void)removeAllObjects;
+
+- (void)trimToCount:(NSUInteger)count;
+
+- (void)trimToCost:(NSUInteger)cost;
+
+- (void)trimToAge:(NSTimeInterval)age;
+
+
+@end
+
+
 @protocol HYImageCache <NSObject>
 
 - (void)addImageForKey:(NSString *)URLIdentifier Image:(UIImage *)image;
@@ -22,6 +73,8 @@
 @end
 
 @interface HYImageCache : NSObject<HYImageCache>
+
+@property (nonatomic,strong)HYMemoryCache *memoryCache;
 
 @property (nonatomic,assign) UInt64 memoryCapacity;
 
--- HYImageDownloader/HYImageDownloader/HYImageCache.m
+++ HYImageDownloader/HYImageDownloader/HYImageCache.m
@@ -7,6 +7,107 @@
 //
 
 #import "HYImageCache.h"
+#import <CoreFoundation/CoreFoundation.h>
+#import <pthread.h>
+
+static inline dispatch_queue_t HYMemoryCacheReleaseQueue(){
+    return dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0);
+}
+
+@interface _HYLinkedMapNode : NSObject {
+    @package
+    __unsafe_unretained _HYLinkedMapNode *_prev; // retained by dic
+    __unsafe_unretained _HYLinkedMapNode *_next; // retained by dic
+    id _key;
+    id _value;
+    NSUInteger _cost;
+    NSTimeInterval _time;
+}
+@end
+
+@implementation _HYLinkedMapNode
+@end
+
+
+@interface _HYLinkedMap : NSObject {
+    @package
+    CFMutableDictionaryRef _dic; // do not set object directly
+    NSUInteger _totalCost;
+    NSUInteger _totalCount;
+    _HYLinkedMapNode *_head; // MRU, do not change it directly
+    _HYLinkedMapNode *_tail; // LRU, do not change it directly
+    BOOL _releaseOnMainThread;
+    BOOL _releaseAsynchronously;
+}
+@end
+
+@implementation _HYLinkedMap
+
+
+
+@end
+
+@implementation HYMemoryCache{
+    pthread_mutex_t _lock;
+    _HYLinkedMap *_lru;
+    dispatch_queue_t _queue;
+}
+
+- (instancetype)init{
+    if (self = [super init]) {
+    pthread_mutex_init(&_lock, NULL);
+        _lru = [_HYLinkedMap new];
+        _queue = dispatch_queue_create("com.yang.cache.memory", DISPATCH_QUEUE_SERIAL);
+        _countLimit = NSUIntegerMax;
+        _costLimit = NSUIntegerMax;
+        _ageLimit = DBL_MAX;
+        _autoTrimInterval = 5.0;
+        _shouldRemoveAllObjectsOnMemoryWarning = YES;
+        _shouldRemoveAllObjectsWhenEnteringBackground = YES;
+        [self _trimRecursively];
+        
+    }
+    return self;
+}
+
+- (void)_trimRecursively{
+    __weak typeof (self) weakSelf = self;
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(_autoTrimInterval * NSEC_PER_SEC)), dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0), ^{
+        __strong typeof(weakSelf) self = weakSelf;
+        if (!self) return;
+        [self _trimInBackground];
+        [self _trimRecursively];
+
+    });
+}
+
+
+- (void)_trimInBackground{
+    
+    dispatch_async(_queue, ^{
+        [self _trimToCost:self->_costLimit];
+        [self _trimToCount:self->_countLimit];
+        [self _trimToAge:self->_ageLimit];
+    });
+}
+
+
+- (void)_trimToCost:(NSUInteger)costLimit{
+    
+}
+
+- (void)_trimToCount:(NSUInteger)countLimit {
+
+}
+
+- (void)_trimToAge:(NSTimeInterval)ageLimit {
+
+}
+
+@end
+
+
+
 
 @interface HYImageCache ()
 @property (nonatomic,strong)NSMutableDictionary *cachedImages;
@@ -24,11 +125,7 @@
 
 -(instancetype)initWithMemoryCapacity:(UInt64)memoryCapacity preferredMemoryCapacity:(UInt64)preferredMemoryCapacity{
     if (self = [super init]) {
-        
-        _memoryCapacity = memoryCapacity;
-        _preferredCapacity = preferredMemoryCapacity;
-        _cachedImages = [NSMutableDictionary dictionary];
-        _synchorinizationQueye = dispatch_queue_create([@"cacheQueue.heyang.com" cStringUsingEncoding:NSASCIIStringEncoding], DISPATCH_QUEUE_CONCURRENT);
+        _memoryCache = [[HYMemoryCache alloc] init];
     }
     
     return self;
