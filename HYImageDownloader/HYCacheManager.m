e0f1bf0b927274920a152849c11f69736b863901
Yang9322 <576421791@qq.com>
2016-12-13 11:25:51 +0800

    Add a manager to control cahce

---
+++ HYImageDownloader/HYCacheManager.m
@@ -0,0 +1,22 @@
+//
+//  HYCacheManager.m
+//  HYImageDownloader
+//
+//  Created by He yang on 2016/12/13.
+//  Copyright © 2016年 He yang. All rights reserved.
+//
+
+#import "HYCacheManager.h"
+
+@implementation HYCacheManager
+
+
+-(void)setObject:(id)obj withKey:(NSString *)key{
+    
+}
+
+-(id )objectForKey:(NSString *)key{
+    return nil;
+}
+
+@end
